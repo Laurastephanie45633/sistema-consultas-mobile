0dc9c5273d2dcda2064617aa0adfdcec6e9e09d3
Laura Oliveira <lauravasquez45633@gmail.com>
2026-09-17 10:57:50 -0300

    Aula 17/09/2026 - Organizando o projeto: catálogo, login e Home

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,6 @@
-export { default as Home } from "./Home";
 export { default as Admin } from "./Admin";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Cadastro } from "./Cadastro";
+export { default as Agendar } from "./Agendar";
 
